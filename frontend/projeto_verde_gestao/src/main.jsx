3f77b6cda4e82c4302e105311fcb2249c9c75aae
Gabriel Saraiva <saraivadegouveiagabriel@gmail.com>
2025-11-09 18:14:05 -0300

    fix: reestruturando o projeto, conforme o recomendado para o padrão de projetos react.

--- frontend/projeto_verde_gestao/src/main.jsx
+++ frontend/projeto_verde_gestao/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import TelaInicial from "./TelaInicial.jsx";
+import "./styles/index.css";
+import TelaInicial from "./pages/Home/TelaInicial.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
